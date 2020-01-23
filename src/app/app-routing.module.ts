96a26b1274fd7d666b55afb3130b073fb03d3c03
sredorta <sergi.redorta@hotmail.com>
2020-01-23 12:32:09 +0100

    Working on scroll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },  
+  {
+    path: 'blog',
+    component: BlogComponent,
+    pathMatch: 'full'
+  }, 
+  {
+    path: 'contact',
+    component: ContactComponent,
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{initialNavigation:'enabled'})],
+  imports: [RouterModule.forRoot(routes,{initialNavigation:'enabled'})],//, scrollPositionRestoration:'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule, TransferState, StateKey, makeStateKey, BrowserTransferStateModule } from '@angular/platform-browser';
+import { NgModule, Inject, PLATFORM_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -45,16 +45,26 @@ import {MatAutocompleteModule,
   //MatDialogRef,
   //MatBottomSheetRef
 } from '@angular/material';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { Router, Scroll, RouterEvent } from '@angular/router';
+import { ViewportScroller, isPlatformServer} from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule,
+    BrowserTransferStateModule,
     [  MatAutocompleteModule, //MATERIAL DESIGN
       MatBadgeModule,
       MatBottomSheetModule,
@@ -95,4 +105,37 @@ import {MatAutocompleteModule,
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    router: Router, 
+    viewportScroller: ViewportScroller,
+    private transferState: TransferState,
+    @Inject(PLATFORM_ID) private _platformId: any
+    ) {
+      //We save a key called transfer-server to detect if it's first time we get into browser comming from server
+      // if it's the case we don't touch scroll to make smooth transition server/browser
+      const key: StateKey<boolean> = makeStateKey<boolean>('scroll');
+      let isFirstBrowser : boolean = false;
+      if (isPlatformServer(this._platformId)) {
+        this.transferState.set(key, true);
+      } else {
+        isFirstBrowser = this.transferState.get(key, false);
+      }
+      console.log("isFirstBrowser returns", isFirstBrowser);
+
+    router.events.subscribe(e => {
+      if (e instanceof Scroll) {
+        if (e.position) {
+          // backward navigation
+          viewportScroller.scrollToPosition(e.position);
+        } else if (isFirstBrowser) {
+          // Do not touch scroll if we come from server
+          isFirstBrowser = false;
+        } else {
+          // forward navigation
+          viewportScroller.scrollToPosition([0, 0]);
+        }
+      }
+    })
+  }
+ }
